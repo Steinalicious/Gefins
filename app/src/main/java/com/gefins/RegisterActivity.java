83165dabd7c632e40284c117b588e07e43d922af
Steinalicious <aso40@hi.is>
2019-02-28 19:09:57 +0000

    registration vistast í db

--- app/src/main/java/com/gefins/RegisterActivity.java
+++ app/src/main/java/com/gefins/RegisterActivity.java
@@ -10,20 +10,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
 public class RegisterActivity extends AppCompatActivity {
 
     //Skilgreiningar
     private Button registerBtn;
 
-    private EditText emailEdTxt;
-    private EditText passEdTxt;
-    private EditText confirmEdTxt;
+    private EditText nameEdTxt, emailEdTxt, passEdTxt, confirmEdTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        nameEdTxt = findViewById(R.id.usernameEditText);
         emailEdTxt = findViewById(R.id.registerEmailEditText);
         passEdTxt = findViewById(R.id.registerPassEditText);
         confirmEdTxt = findViewById(R.id.registerPassConfEditText);
@@ -36,18 +43,41 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick (View v) {
 
                 // Nær í textan frá input field sem eru á register skjá
-                // aka tölvupóstinn, lykillorðið og endurtekninguna
+                // aka notandanafn, tölvupóstinn, lykillorðið og endurtekninguna
+                String name = nameEdTxt.getText().toString();
                 String email = emailEdTxt.getText().toString();
                 String password = passEdTxt.getText().toString();
                 String passwordConfirm = confirmEdTxt.getText().toString();
 
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                Intent intent = new Intent(RegisterActivity.this, UserActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            } else {
+                                android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register failed")
+                                        .setNegativeButton("Retry",null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+
                 if (password.equals(passwordConfirm)) {
-
-                    //Fer frá Registerskjá á Login skjá
-                    Intent intent = new Intent(RegisterActivity.this, UserActivity.class);
-                    startActivity(intent);
+                    RegisterRequest registerRequest = new RegisterRequest(name, email, password, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    queue.add(registerRequest);
                 } else {
-
                     //Varar við að Lykilorð eru mismunandi
                     AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
                     builder.setMessage("Mismundandi lykilorð")
