cb216c2f45d0354a3735bcccd041e41ec9376c87
frokenfreyja <freyjasigur@gmail.com>
2019-03-04 20:44:03 +0000

    dropdown+choose file i newAd

--- app/src/main/java/com/gefins/NavbarActivity.java
+++ app/src/main/java/com/gefins/NavbarActivity.java
@@ -2,9 +2,9 @@ package com.gefins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.widget.DrawerLayout;
-import android.support.design.widget.NavigationView;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.view.GravityCompat;
+import android.support.design.widget.NavigationView;
 
 
 import android.os.Bundle;
