02dae1d0e575e9804f296088c3fbbbc04831f437
frokenfreyja <freyjasigur@gmail.com>
2019-03-04 14:30:43 +0000

    MainActivity utlit

--- app/src/main/java/com/gefins/MainActivity.java
+++ app/src/main/java/com/gefins/MainActivity.java
@@ -1,47 +1,13 @@
 package com.gefins;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Skilgreinigar
-    private Button loginBtn;
-    private Button registerBtn;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        loginBtn = findViewById(R.id.main_login);
-        registerBtn = findViewById(R.id.main_register);
-
-        // Virknin á login takkanum
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //Færir frá aðalskjá yfir á login skjá
-                Intent intent = new Intent(MainActivity.this, UserActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Virknin á register takkanum
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //Færir frá aðalskjá yfir á register skjá
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
     }
 }
