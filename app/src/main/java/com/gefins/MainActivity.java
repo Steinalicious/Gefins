a7cc3fa0b41a7de3a024126c12161c4f9b89d011
frokenfreyja <freyjasigur@gmail.com>
2019-03-04 16:01:00 +0000

    NyAuglysing-skjar

--- app/src/main/java/com/gefins/MainActivity.java
+++ app/src/main/java/com/gefins/MainActivity.java
@@ -1,13 +1,31 @@
 package com.gefins;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createAdBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createAdBtn = findViewById(R.id.createAdButton);
+
+        // Virknin á "skrá auglýsingu" takkanum
+        createAdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Færir frá forsíðu yfir á ný auglýsing skjá
+                Intent intent = new Intent(MainActivity.this, AdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
