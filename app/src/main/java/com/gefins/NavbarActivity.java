99371723cef6246f79b6c3d7c9c579e551dc7d73
Henný Magnúsdóttir <hthm6@hi.is>
2019-03-04 17:25:57 +0000

    menu virkar

---
+++ app/src/main/java/com/gefins/NavbarActivity.java
@@ -0,0 +1,65 @@
+package com.gefins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.view.GravityCompat;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.app.ActionBar;
+
+
+
+public class NavbarActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+    }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    drawerLayout.openDrawer(GravityCompat.END);
+                    return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+
