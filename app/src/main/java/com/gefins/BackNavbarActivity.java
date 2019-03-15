fd2a2d5eb7d6182003929e2c6c424968c9a91258
frokenfreyja <freyjasigur@gmail.com>
2019-03-15 22:36:37 +0000

    login+reg header

--- app/src/main/java/com/gefins/BackNavbarActivity.java
+++ app/src/main/java/com/gefins/BackNavbarActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.widget.TextView;
 
 
 public class BackNavbarActivity extends AppCompatActivity {
