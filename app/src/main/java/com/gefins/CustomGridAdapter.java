cba217ad97ee8ca5d88fb624b18da51653513984
frokenfreyja <freyjasigur@gmail.com>
2019-04-09 22:51:42 +0000

    myndir i viewad+editad

--- app/src/main/java/com/gefins/CustomGridAdapter.java
+++ app/src/main/java/com/gefins/CustomGridAdapter.java
@@ -1,80 +1,47 @@
 package com.gefins;
 
 import android.content.Context;
-import android.util.Log;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
-import Entities.Item;
+public class CustomGridAdapter extends ArrayAdapter {
 
-public class CustomGridAdapter  extends BaseAdapter {
+    private int[] images = new int[]{};
+    private String[] names = new String[]{};
 
-    private List<Item> listData;
-    private LayoutInflater layoutInflater;
-    private Context context;
-
-    public CustomGridAdapter(Context aContext) {
-        this.context = aContext;
-        this.listData = listData;
-        layoutInflater = LayoutInflater.from(aContext);
+    public CustomGridAdapter(@NonNull Context context, int resource,  int[] images, String[] names) {
+        super(context, resource);
+        this.images = images;
+        this.names = names;
     }
 
     @Override
     public int getCount() {
-        return listData.size();
+        return names.length;
     }
 
     @Override
-    public Object getItem(int position) {
-        return listData.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.grid_item_layout, null);
-            holder = new ViewHolder();
-            holder.flagView = (ImageView) convertView.findViewById(R.id.img_ad);
-            new DownloadImg(holder.flagView).execute("https://res.cloudinary.com/aso40/image/upload/v1554385218/avatars-000559149189-tawe7l-t500x500.jpg");
-            holder.countryNameView = (TextView) convertView.findViewById(R.id.ad_title);
-            holder.populationView = (TextView) convertView.findViewById(R.id.ad_zip);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
 
-        Item item = this.listData.get(position);
-        holder.countryNameView.setText(item.getItemName());
-        holder.populationView.setText("" + item.getZipcode());
+        View v;
 
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        return convertView;
-    }
+        v = inflater.inflate(R.layout.grid_item_layout, null);
 
-    // Find Image ID corresponding to the name of the image (in the directory mipmap).
-    public int getMipmapResIdByName(String resName)  {
-        String pkgName = context.getPackageName();
+        TextView textView = (TextView) v.findViewById(R.id.ad_title);
+        ImageView imageView = (ImageView) v.findViewById(R.id.img_ad);
 
-        // Return 0 if not found.
-        int resID = context.getResources().getIdentifier(resName , "mipmap", pkgName);
-        Log.i("CustomGridView", "Res Name: "+ resName+"==> Res ID = "+ resID);
-        return resID;
-    }
+        textView.setText(names[position]);
+        imageView.setImageResource(images[position]);
 
-    static class ViewHolder {
-        ImageView flagView;
-        TextView countryNameView;
-        TextView populationView;
+        return v;
+
     }
 
 }
